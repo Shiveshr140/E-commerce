1f4f434aa45072668cc6922788e5b60a67a176fa
Shiveshr140 <114570394+Shiveshr140@users.noreply.github.com>
2024-08-03 14:27:56 +0530

    Add files via upload

---
+++ views/products/index.js
@@ -0,0 +1,143 @@
+// module.exports = ({products}) =>{
+//     const renderedProducts =  products.map(product=>{
+//         return `
+//          <li>${product.title} - ${product.price} </li>
+//         `
+//     }).join('')
+
+//     return `
+//         <ul> ${renderedProducts}  </ul>
+//     `
+// }
+
+
+
+
+/////************************************ merging more styling
+
+// const layout = require('../layout');
+
+// module.exports = ({ products }) => {
+//   const renderedProducts = products
+//     .map(product => {
+//       return `
+//         <div class="column is-one-quarter">
+//           <div class="card product-card">
+//             <figure>
+//               <img src="data:image/png;base64, ${product.image}"/>
+//             </figure>
+//             <div class="card-content">
+//               <h3 class="subtitle">${product.title}</h3>
+//               <h5>$${product.price}</h5>
+//             </div>
+//             <footer class="card-footer">
+//               <form action="/cart/products" method="POST">
+//                 <button class="button has-icon is-inverted">
+//                   <i class="fa fa-shopping-cart"></i> Add to cart
+//                 </button>
+//               </form>
+//             </footer>
+//           </div>
+//         </div>
+//       `;
+//     })
+//     .join('\n');
+
+//   return layout({
+//     content: `
+//       <section class="banner">
+//         <div class="container">
+//           <div class="columns is-centered">
+//             <img src="/images/banner.jpg" />
+//           </div>
+//         </div>
+//       </section>
+      
+//       <section>
+//         <div class="container">
+//           <div class="columns">
+//             <div class="column "></div>
+//             <div class="column is-four-fifths">
+//               <div>
+//                 <h2 class="title text-center">Featured Items</h2>
+//                 <div class="columns products">
+//                   ${renderedProducts}  
+//                 </div>
+//               </div>
+//             </div>
+//             <div class="column "></div>
+//           </div>
+//         </div>
+//       </section>
+//     `
+//   });
+// };
+
+
+
+///// ******************************************************* submission option
+//// now if i want click on add to cart we must know which product item  is added, one way is <form action="/cart/products/${product.Id}" method="POST">
+//// other way <input type='hidden' value="${product.id}" name="productId"> 
+
+
+const layout = require('../layout');
+
+module.exports = ({ products }) => {
+  const renderedProducts = products
+    .map(product => {
+      return `
+        <div class="column is-one-quarter">
+          <div class="card product-card">
+            <figure>
+              <img src="data:image/png;base64, ${product.image}"/>
+            </figure>
+            <div class="card-content">
+              <h3 class="subtitle">${product.title}</h3>
+              <h5>$${product.price}</h5>
+            </div>
+            <footer class="card-footer">
+            <form action="/cart/products" method="POST">
+            <input type='hidden' value="${product.id}" name="productId">   
+                <button class="button has-icon is-inverted">
+                  <i class="fa fa-shopping-cart"></i> Add to cart
+                </button>
+              </form>
+            </footer>
+          </div>
+        </div>
+      `;
+    })
+    .join('\n');
+
+  return layout({
+    content: `
+      <section class="banner">
+        <div class="container">
+          <div class="columns is-centered">
+            <img src="/images/banner.jpg" />
+          </div>
+        </div>
+      </section>
+      
+      <section>
+        <div class="container">
+          <div class="columns">
+            <div class="column "></div>
+            <div class="column is-four-fifths">
+              <div>
+                <h2 class="title text-center">Featured Items</h2>
+                <div class="columns products">
+                  ${renderedProducts}  
+                </div>
+              </div>
+            </div>
+            <div class="column "></div>
+          </div>
+        </div>
+      </section>
+    `
+  });
+};
+
+
+
